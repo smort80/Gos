6ecdbb903eb334f884c83c0515df0c51faaa548d
Lonsemaria <aekmen@ku.edu.tr>
2016-04-13 21:55:08 +0300

    Create Language.lua

---
+++ Script/Language.lua
@@ -0,0 +1,50 @@
+    local loc_eng = {
+ --General self.Config(Combo) // 17 
+  "[" .. myHero.charName.. "] - Combo Settings", "Use Q in Combo", "Use W in Combo", "Use E in Combo", "Use R in Combo" ,
+  "Use Ignite if target killable","Combo logic","Mana Manager %",
+  "R logic laning phase (1vs1)","Smart","If target killable","Direcly","Use R for multiple enemy","Minimum Enemy to use R ","Use Q if Mana Percent => % " ,"Use E if Mana Percent => % " ,
+  "Use R if Mana Percent => % ",
+
+  --General self.Config(Harass) // 5 // 18
+  "[" .. myHero.charName.. "] - Harass Settings","Harass With Q","Harass With W","Harass With E","Harass With R","           [Mana Manager]",
+  --General self.Config(Harass) // 23 // 24
+  "[" .. myHero.charName.. "] - Farm Settings","[" .. myHero.charName.. "] - Laneclear Settings","Use Q on Laneclear","Use E on Laneclear","Q logic for LaneClear ;",
+  "Only if killable ","[" .. myHero.charName.. "] - Jungleclear Settings","Q logic for JungleClear ;","Use W on JungleClear","Use E on JungleClear","Use Q on JungleClear","Use W if Mana Percent => % " ,
+  "[" .. myHero.charName.. "] - Lasthit Settings","Use Q on Lasthit","Use E on Lasthit","Lasthit Logic","Use Auto Lasthit","Always","Never",
+    --General self.Config(Harass) // 41 // 43}
+ "[" .. myHero.charName.. "] - Escape Settings","Use Q While Escape",
+     --General self.Config(Harass) // 43 // 45}
+"[" .. myHero.charName.. "] - KillSteal Settings ","Killsteal On/Off","Steal With Q","Steal With E","Steal With R","Steal With Ignite",
+          --General self.Config(Harass) // 49 // 51}
+ "[" .. myHero.charName.. "] - Item Settings","Auto Potion","Use Auto Potion", "Auto Potion if Health under -> %","Tiamat Settings","Use Tiamat in combo","Use Tiamat in clear","Tiamat logic for clear",
+"Always","Depends on Minion Count","Minimum Minion to Tiamat","Ravenous Hydra Settings","Use Ravenous Hydra in combo ","Use Ravenous Hydra in clear ","Ravenous Hydra logic for clear",
+"Minimum Minion to Ravenous Hydra","Titanic Hydra Settings", "Use Titanic Hydra in combo","Use Titanic Hydra in clear","Titanic Hydra logic for clear","Minimum Minion to Titanic Hydra" ,"Bilgewater Settings",
+"Use Bilgewater Cutlass","Youmuu's Ghostblade Settings","Use in combo" ,"Use after gapclose","Minimum distance ( Gap close )" ,"Blade of the Ruined King Settings","Use BOTRK","Randuin's Omen Settings",
+"Use Randuin's Omen","Minimum Enemy to Randuin's Omen","QSS settings","Use QSS",
+          --General self.Config(Harass) // 80 // 83}
+"[" .. myHero.charName.. "] - Auto W Settings","Use Auto W ( Riposte )","Only On Dangerous Spells","Skill Danger Slide" ,"Use On Everything if my hp <%","Use On Everything if my hp <%","Use On Auto Attack if my hp <%",
+"Use On Auto Attack if my hp <%","Draw Spells","Spell Settings","Spell List ;" ,"No Skillshot Supported" ,
+ --General self.Config(Harass) // 92 // 95}
+ --General self.Config(Harass) // 92 // 97}
+"[" .. myHero.charName.. "] - Misc Settings","[" .. myHero.charName.. "] - Turret Dive Settings","Use Turret Dive Settings","Turret Dive Mode","Normal Mode","Krystra Mode","Draw Turret Range",
+"Normal Mode Settings >","Minimum Number of Ally Minions" ,"Do not dive if my health > % ","Krystra Mode Settings >"  , "Minimum Number of Ally Minions","Minimum Number of Ally Champions"
+,"Do not dive if my health > % ","[" .. myHero.charName.. "] - Wall Jump Settings","Use Dynamic Wall Jump","Draw wall jump if key pressed",
+"[" .. myHero.charName.. "] - Skinhack Settings","Use Skin Hack","Select Skin","[" .. myHero.charName.. "] - AutoLevel Settings",
+"Use Auto Level", "Auto Level logic","Focus Q>W>E", "Focus Q>E>W","Focus W>Q>E","Focus W>E>Q","Focus E>W>Q", "Focus E>Q>W", "Smart" ,"[" .. myHero.charName.. "] - AntiAFK Settings","Use Anti Afk",
+ --General self.Config(Harass) // 123 // 128}
+ "[" .. myHero.charName.. "] - Extra Settings","Orbwalker Settings","Current Orbwalker :","Prediction Settings","Current Predictions :",
+ "Open Prediction","IPrediction","Gos Prediction","Hitchance Settings","Open Prediction Hitchance","W Hitchance ","    Currently Open Prediction " ,"      is best with this script",
+ --General self.Config(Harass) // 160 // 163}
+"[" .. myHero.charName.. "] - Key Settings","    [Combo Key Settings]","Combo Key","    [Harass Key Settings]",
+  "Smart Harass Key","      [Clear Key Settings]","LaneClear Key","JungleClear Key","Lasthit Key",
+  "      [Other Key Settings]","Escape Key","Wall jump Key","Make Sure That Your Orbwalker"  , " Keys are Same As Here",
+  "[" .. myHero.charName.. "] -Select Language","English","Turkish","German","           "..Scriptname.."" ,
+  "         Script Version:  "..Version.. "  ","   Script was made by  "..Author.. "" ,"Leauge Of Legends Version: "..LVersion.. "" ,"Click For Instructions",
+ --General self.Config(Harass) // 181 // 184}
+ "[" .. myHero.charName.. "] - Draw Settings","Skill Drawing Settings","Q Skill Drawings","W Skill Drawings","E Skill Drawings","Auto Attack Range",
+ "Color Settings","Q Color","W Color","E Color","Vital Spot Color","Auto Attack Color",
+ "Draw Width Settings","Auto Attack Width","Q Skill Width","W Skill Width","E Skill Width","Draw Vital Spots on Target","Draw Damage Indicator","Target calculation",
+"Draw Target Selector Text",
+ --General self.Config(Harass) // 181 // 182}
+"[" .. myHero.charName.. "] - Target Selector","Focus Selected","Select Mode ;","Less Cast", "Less Cast Priority", "Priority", "Most AP", "Most AD", "Closest", "Near Mouse", "Lowest Health", "Lowest Health Priority",
+    }
